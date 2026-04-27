5a077e0974263e54fc5bb6e0124eeb8d8e9d1e10
Aniko0619 <fulopne.kiss.aniko@gmail.com>
2026-04-27 09:56:59 +0200

    EdzesController, NyeuniApplication, Exception tesztek

--- src/test/java/com/nye/edzoterem/nyeuni/NyeuniApplicationTests.java
+++ src/test/java/com/nye/edzoterem/nyeuni/NyeuniApplicationTests.java
@@ -1,13 +1,27 @@
+// Integrációs teszt
 package com.nye.edzoterem.nyeuni;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class NyeuniApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
 	}
 
+	@Test
+	void mainMethod_ShouldRunWithoutExceptions() {
+		NyeuniApplication.main(new String[]{});
+		assertThat(true).isTrue();
+	}
 }
